19fb4c546576a3b1f9b3ed89d2576f5537c0935b
kristen-howton <kristen.howton@gmail.com>
2020-03-25 09:36:00 -0500

    added comments noteProvider, note, noteList, also changed css for notes and statements

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -4,6 +4,11 @@ export const Note = (noteObject, criminalObject) => {
             <p>${noteObject.noteText}</p>
             <p>${new Date(noteObject.timestamp).toLocaleDateString()}</p>
             <p>${criminalObject.name}</p>
+            <p>
+                <button id="deleteNote--${noteObject.id}">Delete Note</button>
+            </p>
         </section>
     `
 }
+
+//Note takes two paramters(place holders) and retuns a string of HTML of what a note will look like on the DOM
--- scripts/notes/NoteProvider.js
+++ scripts/notes/NoteProvider.js
@@ -1,7 +1,11 @@
+//empty array that will store the note objects
+
 let notes = []
 
+//goes to DOM(index.html) and finds the first element with the class of container. Use eventHub for events
 const eventHub = document.querySelector(".container")
 
+//A custom event(event we create) to let other modules know that the POST occured (notes state changed) and yells it out for other modules that we let know
 const dispatchStateChangeEvent = () => {
     const noteStateChangedEvent = new CustomEvent("noteStateChanged")
 
@@ -9,12 +13,13 @@ const dispatchStateChangeEvent = () => {
 }
 
 /*
-    Allow other modules to get a copy of the application state
+    Allow other modules to get a copy of the application state, sorts the notes in order of when they were posted
 */
-export const useNotes = () => notes.sort((c,n) => n.timestamp - c.timestamp).slice()
+export const useNotes = () => notes.sort((current,next) => next.timestamp - current.timestamp).slice()
 
 /*
-    Get the state of the notes from the API into the application
+    Get the state of the notes from the API into the application, tell me all of the objects that you have
+    fills up the empty array on line 1 with the parsed notes
 */
 export const getNotes = () => {
     return fetch('http://localhost:8088/notes')
@@ -23,6 +28,12 @@ export const getNotes = () => {
             notes = parsedNotes
         })
 }
+
+/*
+    saveNote takes a note object and turns it into JSON string, POST string to notes API
+    then it getNotes 
+    then lets other modules know that the notes state has been updated with a dispatchStateChangeEvent
+*/
 
 export const saveNote = note => {
     return fetch('http://localhost:8088/notes', {
